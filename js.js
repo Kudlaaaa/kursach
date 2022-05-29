65fa69c936ab2f7f12bc486a1fbb5ce40dacfdfa
Kudlaaaa <Ulyana.Kudlay@gmail.com>
2022-05-29 19:53:12 +0300

    main page

---
+++ js.js
@@ -0,0 +1,16 @@
+/*document.onwheel = function(event){
+    if(event.deltaY > 0){
+        window.scrollBy({
+            top: window.innerHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+    else{
+        window.scrollBy({
+            top: -window.innerHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+}*/
