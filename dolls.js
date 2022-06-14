fab8494a92216e8f9e95cb6a608f79729f1b16d6
Kudlaaaa <Ulyana.Kudlay@gmail.com>
2022-06-14 15:15:11 +0300

    json

---
+++ dolls.js
@@ -0,0 +1,20 @@
+$('document').ready(function(){
+    loadGoods();
+});
+
+function loadGoods(){
+    $.getJSON('db.json', function(data){
+        var htmlOut = '';
+        for(var key in data){
+            htmlOut+='<div class="single-doll">';
+            htmlOut+='<div class="single-doll-img">';
+            htmlOut+='<a href="'+data[key]['link']+'" target="_blank">';
+            htmlOut+='<img src="'+data[key]['image']+'" alt="Кукла">';
+            htmlOut+='<div class="single-doll-img-layhtmlOut"></div>';
+            htmlOut+='<p class="buy-button">Заказать</p></a></div>';
+            htmlOut+='<p>'+data[price]+'</p>';
+            htmlOut+='</div>';
+        }
+        $('#dolls').html(htmlOut);
+    });
+}
